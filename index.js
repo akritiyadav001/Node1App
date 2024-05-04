9781908da4961b466b95e6bdcbe533fcc647ca18
Akriti Yadav <gdakriti.yadav001@gmail.com>
2024-05-04 13:23:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+//import express from "express" 
+require('dotenv').config()
+
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/facebook', (req, res) =>{
+    res.send('Welcome to the facebook page.')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
